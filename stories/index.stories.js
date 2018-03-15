eb166450a15b58253f681153e630fcefda240897
Bartosz Markowski <nenros@gmail.com>
2018-03-15 13:27:49 +0100

    2 krok

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,12 +2,39 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import ActivityIcon from "../src/components/ActivityIcon";
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+import { Stations } from "../src/containers/Stations";
+
+
+
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Activity icon', module)
+  .add('true state', () => <ActivityIcon active={true} />)
+  .add('false state', () => <ActivityIcon active={false} />)
+
+const stationsDefaultProps = {
+  fetchStations: () => null,
+  stations: {
+    error: null,
+    records: []
+  },
+  children: <div>Children</div>
+}
+
+const stationsErrorProps = {
+  ...stationsDefaultProps,
+  stations: {
+    ...stationsDefaultProps.stations,
+    error: "Error"
+  }
+}
+
+storiesOf('Stations', module)
+  .add('show stations', () => <Stations {...stationsDefaultProps} />)
+  .add('show error', () => <Stations {...stationsErrorProps} />)
+
+storiesOf('Card', module)
